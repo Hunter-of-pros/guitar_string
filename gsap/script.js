5ab500d3b7e178491606412fb73c95fd820a148d
Hunter-of-pros <vikasm8660@gmail.com>
2025-11-01 05:48:12 +0530

    Initial commit

---
+++ gsap/script.js
@@ -0,0 +1,21 @@
+var initialPath = 'M 10 100 Q 250 100 490 100';
+var finalPath = 'M 10 100 Q 250 100 490 100';
+var string = document.querySelector("#string")
+string.addEventListener("mousemove", function(dets) {
+    path = `M 10 100 Q ${dets.x} ${dets.y} 490 100`;
+    gsap.to("svg path",
+        {attr:{d: path},
+        duration: 0.3,
+        ease:"power3.out"
+    }
+    )
+})
+string.addEventListener("mouseleave", function(){
+    gsap.to("svg path",
+        {attr:{d: finalPath},
+        duration: 0.7,
+        ease: "elastic.out(2,0.1)"
+
+        })
+    
+})
